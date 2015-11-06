4a513ba742e0bae960281297bd7f3158c2e34076
619561504 <619561504@qq.com>
2015-11-06 18:39:13 +0800

    tt

--- src/main/java/com/wutianyi/fnlp/Example.java
+++ src/main/java/com/wutianyi/fnlp/Example.java
@@ -31,5 +31,8 @@ public class Example {
 		HashMap<String, String> ret = factory
 				.ner("詹姆斯·默多克和丽贝卡·布鲁克斯 鲁珀特·默多克旗下的美国小报《纽约邮报》的职员被公司律师告知，保存任何也许与电话窃听及贿赂有关的文件。");
 		System.out.println(ret);
+
+		String sentence = "晓莉女士：您个人信用卡已过缴款截止日，本期仍应还￥18,326.65，最低1,535.98，点 t.cn/RAkRMyW 还款[招商银行]";
+		System.out.println(factory.tag2String(sentence));
 	}
 }
---
+++ src/main/java/com/wutianyi/stanford/Usage2.java
@@ -0,0 +1,35 @@
+/**
+ * 2015年11月3日hanjiewu
+ */
+package com.wutianyi.stanford;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.ChineseSegmenterAnnotator;
+import edu.stanford.nlp.util.CoreMap;
+
+/**
+ * @author hanjiewu
+ *
+ */
+public class Usage2 {
+	public static void main(String[] args) throws IOException {
+		Properties props = new Properties();
+		props.load(Usage1.class.getClassLoader().getResourceAsStream("StanfordCoreNLP-chinese.properties"));
+		ChineseSegmenterAnnotator cAnnotator = new ChineseSegmenterAnnotator("segment", props);
+		String text = "您尾号 {1} 的[*n*6]卡白金卡信用额度为 {2} 元人民币或等值外币，当前可用额度为 {3} 元人民币或等值外币。 回复“应还”可查当前欠款。微信也可查询账户信息啦，赶快关注“中国光大银行”[光大银行]";
+		Annotation document = new Annotation(text);
+		cAnnotator.annotate(document);
+		for (CoreLabel coreLabel : document.get(TokensAnnotation.class)) {
+			String word = coreLabel.get(TextAnnotation.class);
+			System.out.println(word);
+		}
+	}
+}
