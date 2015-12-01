2720f465d8e6ba27ef1d8f187a64cf4c93af4b8b
619561504 <619561504@qq.com>
2015-12-01 20:56:25 +0800

    添加spring batch例子

---
+++ src/main/java/com/wutianyi/batch/ProductItemProcessor.java
@@ -0,0 +1,24 @@
+/**
+ * hanjiewu 
+ * 2015年12月1日:下午8:48:39
+ */
+package com.wutianyi.batch;
+
+import org.springframework.batch.item.ItemProcessor;
+
+/**
+ * @author hanjiewu
+ *
+ */
+public class ProductItemProcessor implements ItemProcessor<Product, Product> {
+
+	/* 
+	 * 
+	 */
+	@Override
+	public Product process(Product item) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
